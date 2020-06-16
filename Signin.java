68b3608de1e66cff3ab047315e46842f7ada59ce
liu <liu@qq.com>
2020-06-16 09:37:19 +0800

    在公司开发签到功能

---
+++ Signin.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-16 09:34:57
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-16 09:36:01
+*/
+
+public class Signin{
+    public static void main(String[] args) {
+
+    }
